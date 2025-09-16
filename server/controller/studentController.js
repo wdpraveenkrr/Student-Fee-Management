1ab6f4312fc5e0499c1aa0b97eefd9eb0b630313
Praveen Kumar <159166092+wdpraveenkrr@users.noreply.github.com>
2025-09-16 19:07:47 +0530

    Update studentController.js

--- server/controller/studentController.js
+++ server/controller/studentController.js
@@ -34,8 +34,8 @@ export const register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
@@ -86,8 +86,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
@@ -110,8 +110,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie('token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict',
+      secure: true,
+      sameSite: "None",
     })
 
     return res.json({ success: true, message: "Logged Out" })
